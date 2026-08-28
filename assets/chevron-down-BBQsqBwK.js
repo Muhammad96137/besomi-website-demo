4c0c21574279a690d3b421de675c5f20704385b9
MuhammadHalabi <muhammad.halabi.137@gmail.com>
2026-08-28 06:28:29 +0400

    Bigger marquee logos

--- assets/chevron-down-BBQsqBwK.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./index-Dn1v7Vpu.js";const n=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],c=o("chevron-down",n);export{c as C};
---
+++ assets/chevron-left-DtzoyXQb.js
@@ -0,0 +1,1 @@
+import{h as e}from"./index-CSNKM0Qx.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],c=e("chevron-left",o);export{c as C};

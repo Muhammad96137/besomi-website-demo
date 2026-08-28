62050276dc43ead9c53c72167c29851237ca925f
MuhammadHalabi <muhammad.halabi.137@gmail.com>
2026-08-28 06:37:05 +0400

    About banner real photos

--- assets/check-DtsuyLrE.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./index-CSNKM0Qx.js";const a=[["path",{d:"M12 7v14",key:"1akyts"}],["path",{d:"M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z",key:"ruj8y"}]],t=o("book-open",a);const c=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],h=o("check",c);export{t as B,h as C};
